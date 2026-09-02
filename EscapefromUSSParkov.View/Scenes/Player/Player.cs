ea0d8e6ffff514d8d1cead1236f8926b15ab4466
SacrificiaI <nicholaschan225@gmail.com>
2026-09-02 21:44:04 +1000

    Added aiming line and bullet .png

--- EscapefromUSSParkov.View/Scenes/Player/Player.cs
+++ EscapefromUSSParkov.View/Scenes/Player/Player.cs
@@ -12,6 +12,8 @@ public sealed partial class Player : CharacterBody2D
     [Export] private CollisionShape2D _collision;
     [Export] private Camera2D _camera;
 
+    [Export] private Line2D _aimLine;
+
     // Camera clamping variables to prevent camera from exiting level borders
     [Export] private int _cameraLeft = -5000000;
     [Export] private int _cameraRight = 5000000;
@@ -24,11 +26,20 @@ public sealed partial class Player : CharacterBody2D
     public override void _Ready()
     {
         SetLimits();
+        _aimLine.AddPoint(Vector2.Zero);
+        _aimLine.AddPoint(Vector2.Zero);
     }
 
     public override void _Process(double delta)
     {
         _direction = Input.GetVector("move_left", "move_right", "move_up", "move_down");
+
+        bool aiming = Input.IsActionPressed("aim");
+        _aimLine.Visible = aiming;
+        if (aiming)
+        {
+            _aimLine.SetPointPosition(1, ToLocal(GetGlobalMousePosition()));
+        }
     }
 
     public override void _PhysicsProcess(double delta)
